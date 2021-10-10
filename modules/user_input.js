69c1d769fadaef94fe2b037a4a12858cc9a357a5
Daniel Franke <daniel.franke19@gmail.com>
2021-10-10 14:07:38 +0200

    Presseausweis hinzugefuegt, styling fur 2k mintor

--- modules/user_input.js
+++ modules/user_input.js
@@ -1,11 +1,11 @@
 const { main } = require("../main");
 
-let element = document.getElementById('name-input-btn')
+let element = document.getElementById('presseausweis-input-button')
 element.addEventListener('click',user_input,false);
 
 function user_input()
 {
-    let name = document.getElementById("name-input");
+    let name = document.getElementById("presseausweis-input-name");
     document.cookie = name.value;
     main()
 }
